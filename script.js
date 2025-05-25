b9851eb1cc00c6b8099a0613573a7895498cd9cc
Md.Naahe Uddin Laskar <123208141+naahe25@users.noreply.github.com>
2025-05-25 13:45:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
- 
-
 let counter = 1;
 
 document.getElementById("userForm").addEventListener("submit", function (event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
+  // Input values
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
+  const age = document.getElementById("age").value;
+  const phone = document.getElementById("phone").value;
 
+  // Add to table
   const tableBody = document.getElementById("userTableBody");
-
   const newRow = document.createElement("tr");
 
   newRow.innerHTML = `
     <td class="border px-4 py-2">${counter++}</td>
     <td class="border px-4 py-2">${name}</td>
     <td class="border px-4 py-2">${email}</td>
+    <td class="border px-4 py-2">${age}</td>
+    <td class="border px-4 py-2">${phone}</td>
   `;
 
   tableBody.appendChild(newRow);
 
-  
+  // Reset form
   document.getElementById("userForm").reset();
 });
